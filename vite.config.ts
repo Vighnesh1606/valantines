66ef9554da57aa23e64e426241eba7fcc46f44b4
Vighnesh1606 <115088157+Vighnesh1606@users.noreply.github.com>
2026-02-07 22:19:32 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => {
       server: {
         port: 3000,
         host: '0.0.0.0',
-          allowedHosts: 'valantines-4.onrender.com'
+          allowedHosts: 'valantines-6.onrender.com',
       },
       plugins: [react()],
       define: {
