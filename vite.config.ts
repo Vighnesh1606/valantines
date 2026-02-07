616d3326d64b056679a0daa4bf799678f9e55685
Vighnesh1606 <115088157+Vighnesh1606@users.noreply.github.com>
2026-02-07 22:13:07 +0530

    Restrict allowedHosts to specific domain

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => {
       server: {
         port: 3000,
         host: '0.0.0.0',
-          allowedHosts: 'all'
+          allowedHosts: 'valantines-4.onrender.com'
       },
       plugins: [react()],
       define: {
